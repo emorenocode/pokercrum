936d00293cc419a9a1ebc3ca268cc560d381c181
Enrique Moreno <emorenocode@gmail.com>
2025-10-21 13:58:08 -0500

    feat: integrate App Check and retrieve token in the App component

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,4 +1,5 @@
 import { Component, inject } from '@angular/core';
+import { AppCheck, getToken } from '@angular/fire/app-check';
 import { Meta } from '@angular/platform-browser';
 import { RouterOutlet } from '@angular/router';
 
@@ -10,11 +11,16 @@ import { RouterOutlet } from '@angular/router';
 })
 export class App {
   private readonly meta = inject(Meta);
+  private appCheck = inject(AppCheck);
 
   constructor() {
     this.meta.updateTag({
       name: 'decription',
       content: 'PokerCrum is a Planning Poker Game for teams using scrum methodology',
     });
+
+    getToken(this.appCheck, true)
+      .then((t) => console.log('✅ AppCheck Token:', t.token))
+      .catch((err) => console.error('❌ Error al obtener token:', err));
   }
 }
