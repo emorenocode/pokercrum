750017596f6577697600151ac9930d0307e0dc27
Enrique Moreno <emorenocode@gmail.com>
2025-10-21 16:53:10 -0500

    refactor: remove unused App Check import and token retrieval from App component

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,4 @@
 import { Component, inject } from '@angular/core';
-import { AppCheck, getToken } from '@angular/fire/app-check';
 import { Meta } from '@angular/platform-browser';
 import { RouterOutlet } from '@angular/router';
 
@@ -11,16 +10,11 @@ import { RouterOutlet } from '@angular/router';
 })
 export class App {
   private readonly meta = inject(Meta);
-  private appCheck = inject(AppCheck);
 
   constructor() {
     this.meta.updateTag({
       name: 'decription',
       content: 'PokerCrum is a Planning Poker Game for teams using scrum methodology',
     });
-
-    getToken(this.appCheck, true)
-      .then((t) => console.log('✅ AppCheck Token:', t.token))
-      .catch((err) => console.error('❌ Error al obtener token:', err));
   }
 }
